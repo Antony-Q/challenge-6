493eff997a8ac8faf65f2451bb5faca738beb8b6
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-03-10 22:10:44 -0600

    editing weather dashboard API request

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,22 @@
-fetch("https://api.github.com/users/Antony-Q/repos").then(function(response) {
-  response.json().then(function(data) {
-    console.log(data);
-  });
-});
+var getUserRepos = function () {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-console.log("outside");
+    // make a request to the url
+    fetch(apiUrl).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data);
+        });
+    });
+};
+var getWeatherAPI = function () {
+    // format the weather dashboard api url
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={c6c3742f8d69c2897e68da43607165dc}";
+
+    // make a request to the url
+    fetch(apiUrl).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data);
+        });
+    });
+};

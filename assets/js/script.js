d48ab6244c38089172cd3f9f4f3032e1d738fc33
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-03-14 00:15:47 -0500

    trying new fetch method

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,43 @@
-var getUserRepos = function () {
-    // format the github api url
-    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+// var getWeatherAPI = function(user) {
+//     // format the weather dashboard api url
+//     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=minutely,alerts,hourly,daily&appid=c6c3742f8d69c2897e68da43607165dc";
+  
+//     // make a get request to url
+//     fetch(apiUrl).then(function(response) {
+//       console.log(response);
+//       response.json().then(function(data) {
+//         console.log(data);
+//       });
+//     });
+//   };
+  
+const api_url = 
+      "https://api.openweathermap.org/data/2.5/onecall?lat=30.27&lon=-97.74&exclude=minutely,alerts,hourly&appid=c6c3742f8d69c2897e68da43607165dc";
+  
+// Defining async function
+async function getWeatherAPI(url) {
+    
+    // Storing response
+    const response = await fetch(url);
+    
+    // Storing data in form of JSON
+    var data = await response.json();
+    console.log(data);
+    if (response) {
+    }
+}
+  
+  getWeatherAPI(api_url);
 
-    // make a request to the url
-    fetch(apiUrl).then(function (response) {
-        response.json().then(function (data) {
-            console.log(data);
-        });
-    });
-};
-var getWeatherAPI = function () {
-    // format the weather dashboard api url
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={c6c3742f8d69c2897e68da43607165dc}";
 
-    // make a request to the url
-    fetch(apiUrl).then(function (response) {
-        response.json().then(function (data) {
-            console.log(data);
-        });
-    });
-};
+
+  // Variables for search query DOM
+  var searchFormEl = document.querySelector("#form-search");
+  var searchInputEl = document.querySelector("#input-search");
+
+  // Function for form submission browser event
+  var formSubmitHandler = function(event) {
+      event.preventDefault();
+      console.log(event);
+  };
+  searchFormEl.addEventListener("submit", formSubmitHandler);

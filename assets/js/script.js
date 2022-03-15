7148d37af6b48d4bf66e076f8bd33fc4e0019809
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-03-15 17:35:23 -0500

    worked out logic for api data to html element

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,43 +10,30 @@
 //       });
 //     });
 //   };
+
+
 $(document).ready(function () {
-    const api_url =
-        "https://api.openweathermap.org/data/2.5/onecall?lat=30.27&lon=-97.74&exclude=minutely,alerts,hourly&appid=c6c3742f8d69c2897e68da43607165dc";
 
+    //
+
+    
     // Defining async function
-    async function getWeatherAPI(url) {
+    async function getWeatherAPI(lat, lon) {
+        const api_url =
+            `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts,hourly&appid=c6c3742f8d69c2897e68da43607165dc`;
         // Storing response
-        const response = await fetch(url);
-
+        const response = await fetch(api_url);
+        
         // Storing data in form of JSON
         var data = await response.json();
         console.log(data);
+
+        // Display data in designated html elements
         $('.data-point-1').html(`<div>Temp: ${data.current.temp}</div>`)
         $('.data-point-2').html(`<div>Feel like: ${data.current.feels_like}</div>`)
         $('.data-point-3').html(`<div>Pressure: ${data.current.pressure}</div>`)
         $('.data-point-4').html(`<div>Day4 wind speed: ${data.daily[4].wind_speed}</div>`)
     }
-
-    getWeatherAPI(api_url);
-
-    // document.body.onload = addElement;
-
-    // function addElement () {
-    //   // create a new div element
-    //   const newDiv = document.createElement("div");
-    
-    //   // and give it some content
-    //   const newContent = document.createTextNode("Hi there and greetings!");
-    
-    //   // add the text node to the newly created div
-    //   newDiv.appendChild(newContent);
-    
-    //   // add the newly created element and its content into the DOM
-    //   const currentDiv = document.getElementById("div1");
-    //   document.body.insertBefore(newDiv, currentDiv);
-    // }
-
     
     // Variables for search query DOM
     var searchFormEl = document.querySelector("#form-search");
@@ -55,10 +42,23 @@ $(document).ready(function () {
     // Function for form submission browser event
     var formSubmitHandler = function (event) {
         event.preventDefault();
-        console.log(event);
+        var searchCity = searchInputEl.value.trim();
+        console.log(searchCity);
+        fetchCoords(searchCity);
     };
     searchFormEl.addEventListener("submit", formSubmitHandler);
+
+    async function fetchCoords(city) {
+        var searchCityAPI = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c6c3742f8d69c2897e68da43607165dc`;
+        const response = await fetch(searchCityAPI);
+
+        // Storing data in form of JSON
+        var data = await response.json();
+        console.log(data);
+        getWeatherAPI(data.coord.lat, data.coord.lon);
+    }
 });
+
 
 
 

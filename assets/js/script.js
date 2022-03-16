418c1c6e854c34723664e3a77c17fcf2bc6151a0
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-03-16 15:44:04 -0500

    mvp for DOM creation display created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,29 @@
-// var getWeatherAPI = function(user) {
-//     // format the weather dashboard api url
-//     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=minutely,alerts,hourly,daily&appid=c6c3742f8d69c2897e68da43607165dc";
-
-//     // make a get request to url
-//     fetch(apiUrl).then(function(response) {
-//       console.log(response);
-//       response.json().then(function(data) {
-//         console.log(data);
-//       });
-//     });
-//   };
-
-
+// JQuery initialization
 $(document).ready(function () {
 
-    //
-
-    
     // Defining async function
     async function getWeatherAPI(lat, lon) {
         const api_url =
             `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts,hourly&appid=c6c3742f8d69c2897e68da43607165dc`;
         // Storing response
         const response = await fetch(api_url);
-        
+
         // Storing data in form of JSON
         var data = await response.json();
         console.log(data);
 
         // Display data in designated html elements
         $('.data-point-1').html(`<div>Temp: ${data.current.temp}</div>`)
-        $('.data-point-2').html(`<div>Feel like: ${data.current.feels_like}</div>`)
-        $('.data-point-3').html(`<div>Pressure: ${data.current.pressure}</div>`)
-        $('.data-point-4').html(`<div>Day4 wind speed: ${data.daily[4].wind_speed}</div>`)
-    }
-    
+        $('.data-point-2').html(`<div>Wind: ${data.current.wind_speed}</div>`)
+        $('.data-point-3').html(`<div>Humidity: ${data.current.humidity}</div>`)
+        $('.data-point-4').html(`<div>UV Index: ${data.current.uvi}</div>`)
+    };
+
     // Variables for search query DOM
     var searchFormEl = document.querySelector("#form-search");
     var searchInputEl = document.querySelector("#input-search");
 
-    // Function for form submission browser event
+    // Function for form submission event
     var formSubmitHandler = function (event) {
         event.preventDefault();
         var searchCity = searchInputEl.value.trim();
@@ -48,6 +32,7 @@ $(document).ready(function () {
     };
     searchFormEl.addEventListener("submit", formSubmitHandler);
 
+    // Defining async function for variable input submission
     async function fetchCoords(city) {
         var searchCityAPI = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c6c3742f8d69c2897e68da43607165dc`;
         const response = await fetch(searchCityAPI);

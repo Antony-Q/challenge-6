28b5a7c0c390e849e368413246b5dc9b806abd6a
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-03-16 17:36:19 -0500

    card data displays correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,10 +13,19 @@ $(document).ready(function () {
         console.log(data);
 
         // Display data in designated html elements using second API request
+
+        // Current weather card
         $('.data-point-1').html(`<div>Temp: ${data.current.temp}</div>`)
         $('.data-point-2').html(`<div>Wind: ${data.current.wind_speed}</div>`)
         $('.data-point-3').html(`<div>Humidity: ${data.current.humidity}</div>`)
         $('.data-point-4').html(`<div>UV Index: ${data.current.uvi}</div>`)
+
+        // Forecast cards
+        $('.future-card-1').html(`<div>Temp: ${data.daily[0].temp.day}<div>Wind: ${data.daily[0].wind_speed}<div>Humidity: ${data.daily[0].humidity}</div></div></div>`)
+        $('.future-card-2').html(`<div>Temp: ${data.daily[1].temp.day}<div>Wind: ${data.daily[1].wind_speed}<div>Humidity: ${data.daily[1].humidity}</div></div></div>`)
+        $('.future-card-3').html(`<div>Temp: ${data.daily[2].temp.day}<div>Wind: ${data.daily[2].wind_speed}<div>Humidity: ${data.daily[2].humidity}</div></div></div>`)
+        $('.future-card-4').html(`<div>Temp: ${data.daily[3].temp.day}<div>Wind: ${data.daily[3].wind_speed}<div>Humidity: ${data.daily[3].humidity}</div></div></div>`)
+        $('.future-card-5').html(`<div>Temp: ${data.daily[4].temp.day}<div>Wind: ${data.daily[4].wind_speed}<div>Humidity: ${data.daily[4].humidity}</div></div></div>`)
     };
 
     // Variables for search query DOM
@@ -51,7 +60,8 @@ $(document).ready(function () {
         setInterval(function () { liveDate.innerHTML = `${moment().format('L')}` }, 1000);
 
         // Display data in designated html elements using first API request
-        $('.city-name').html(`<div>${data.name}${data.weather[0].icon}</div>`)
+        $('.city-name').html(`<div>${data.name}</div>`)
+        $('.weather-icon').html(`<div>${data.weather[0].icon}</div>`)
     }
 });
 
